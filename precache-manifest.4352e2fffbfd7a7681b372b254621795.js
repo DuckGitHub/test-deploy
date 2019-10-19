88a1ba58cf95f197f13b6acd6f0017f63e6542aa
pp <pp@22.com>
2019-10-19 15:23:04 -0300

    Updates

---
+++ precache-manifest.4352e2fffbfd7a7681b372b254621795.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae8a08e78b3e2c29d02a85a0d99e279b",
+    "url": "/test-deploy/index.html"
+  },
+  {
+    "revision": "bfca452a174958264836",
+    "url": "/test-deploy/static/css/2.9028a4ae.chunk.css"
+  },
+  {
+    "revision": "0ca7f09e34fea04bdced",
+    "url": "/test-deploy/static/css/main.73f99708.chunk.css"
+  },
+  {
+    "revision": "bfca452a174958264836",
+    "url": "/test-deploy/static/js/2.71c354e9.chunk.js"
+  },
+  {
+    "revision": "0ca7f09e34fea04bdced",
+    "url": "/test-deploy/static/js/main.17119d0f.chunk.js"
+  },
+  {
+    "revision": "55fe6ac7058d4f093283",
+    "url": "/test-deploy/static/js/runtime-main.5cd29098.js"
+  }
+]);
